394074c1db9c0d6e3e0234ec0e501a30a3da8324
Saeed Abdul Rahim <sae.ar2@gmail.com>
2022-01-30 22:16:17 +0530

    new assignment

---
+++ class_assign3.py
@@ -0,0 +1,36 @@
+"""
+Create class: Student -> Class_Division, Family
+    Variables:
+        name: string
+        class_number: integer
+        division: string
+        gender: string
+        marks: dictionary
+            - science, maths
+        attendance: list / array (10 days)
+            True / False
+    
+    Functions:
+        change_marks | Science / Math
+        update_attendance - insert (True / False)
+        check_pass:
+            check passed in math / science | Score > 40 | -> print
+    
+Create class Class_Division:
+    Variable:
+        student_list: list
+        class_teacher: string
+
+    Function:
+        add_student
+            student_list -> add
+        change_teacher
+        list_all_students:
+            print all students
+
+Create class Family
+    Variable: 
+        father_name: string
+        mother_name: string
+        siblings: list
+"""
